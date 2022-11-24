73459b8660152f92cebd80e367f4c531150c08b6
Elis Antonio Perez <elisperezmusic@gmail.com>
2022-11-24 15:12:40 -0500

    notificaciones

--- 34-APIs-JS/js/01-app.js
+++ 34-APIs-JS/js/01-app.js
@@ -1,9 +1,23 @@
 const notificarBtn = document.querySelector('#notificar');
 
 notificarBtn.addEventListener('click', () => {
-  Notification
-    .requestPermission()
-    .then(result => {
-      console.log('El resultado es:', result); // default | denied | granted
+  // Para preguntar al usuario si desea habilitar las notificaciones en el navegagor:
+  Notification.requestPermission().then(result => {
+    console.log('El resultado es:', result); // default | denied | granted
+  });
+});
+
+const verNotificacionBtn = document.querySelector('#verNotificacion');
+verNotificacionBtn.addEventListener('click', () => {
+  // console.log('click');
+  if (Notification.permission == 'granted') {
+    const notificacion = new Notification('Esta es la notificación', {
+      icon: 'img/ccj.png',
+      body: 'Código Líder también aprende con Código con Juan',
     });
+
+    notificacion.onclick = function () {
+      window.open('https://www.codigoconjuan.com');
+    }
+  }
 });
